45bf7878396948738c3bb8270699a7c048717165
Yassine Elhilali <elhilaliyacin@gmail.com>
2021-08-23 19:51:19 +0100

    Countdown Timer

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,54 @@
+let countdown
+
+const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
+
+function timer(seconds){
+    clearInterval(countdown)
+
+    const now = Date.now()
+    const then = now + seconds * 1000
+
+    displayTimeLeft(seconds)
+    displayEndTime(then)
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000)
+
+        if(secondsLeft < 0){
+            clearInterval(countdown)
+            return
+        }
+
+        displayTimeLeft(secondsLeft)
+    }, 1000)
+}
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds / 60)
+    const reminderSeconds = seconds % 60
+    const display = `${minutes}:${reminderSeconds < 10 ? '0' : ''}${reminderSeconds}`
+    document.title = display
+    timerDisplay.textContent = display
+}
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp)
+    const hours = end.getHours()
+    const minutes = end.getMinutes()
+    endTime.textContent = `Ends at ${hours}:${minutes < 10 ? '0' : ''}${minutes}`
+}
+
+function startTimer(){
+    const seconds = parseInt(this.dataset.time)
+    timer(seconds)
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer))
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const mins = this.minutes.value
+    timer(mins * 60)
+    this.reset()
+})
